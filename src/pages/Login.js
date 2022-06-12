d5fdd69d4b9d10f86d2c674db839e3563ce8b132
tylerasa <sarpongasaresylvester@gmail.com>
2022-06-12 22:35:15 +0000

    +login

---
+++ src/pages/Login.js
@@ -0,0 +1,28 @@
+import React from "react";
+import SelectInput from "../components/Inputs/SelectInput";
+import SubmitButton from "../components/Inputs/SubmitButton";
+import TextInput from "../components/Inputs/TextInput";
+
+const Login = () => {
+  return (
+    <div className="container px-4 py-5  bg-appBg">
+      <h2 className="text-activeButton font-bold text-center py-10 text-3xl">
+        RATTIFY
+      </h2>
+      <div className="sign-up-wrapper bg-white rounded-md shadow-appShadow mx-auto py-5 px-6">
+        <h1 className="font-semibold text-2xl text-center mb-5">
+          Create password
+        </h1>
+        <TextInput label="Email address" type="email" id="email" />
+        <TextInput label="Password" type="password" id="password" />
+
+        <SubmitButton text="Complete sign up" />
+        <p className="text-sm text-center mt-5 font-medium">
+          <a href="/">Go back</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
