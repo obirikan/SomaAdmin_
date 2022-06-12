4fba2fa8f93d6fb478da5e12c610853c338c86ad
tylerasa <sarpongasaresylvester@gmail.com>
2022-06-12 21:05:00 +0000

    +sign up page

---
+++ src/pages/SignUp.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './styles.css'
+const SignUp = () => {
+  return (
+    <div className="container px-4 ">
+      <div className="sign-up-wrapper bg-red-400 mx-auto ">ss</div>
+    </div>
+  );
+};
+
+export default SignUp;
