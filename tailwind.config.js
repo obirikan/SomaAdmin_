0b45bb945368b351c4bd68869f008eb2a241e5fb
tylerasa <sarpongasaresylvester@gmail.com>
2022-06-12 20:52:51 +0000

    +tailwind colors, font config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        appBg: "#E5E5E5",
+        activeButton: "#23574c",
+        inactiveButton: "#91aba5",
+      },
+      fontFamily: {
+        sans: "'Inter', sans-serif",
+      },
+    },
   },
   plugins: [],
-}
+};
