cb7d0b0bd4b375307568aa5c9b983ae86cdb82c6
tylerasa <sarpongasaresylvester@gmail.com>
2022-06-12 22:22:08 +0000

    +accent black

--- src/components/Inputs/Checkbox.js
+++ src/components/Inputs/Checkbox.js
@@ -2,17 +2,16 @@ import React from "react";
 
 const Checkbox = ({ text }) => {
   return (
-    <div class="flex items-center mb-4">
+    <div class="flex items-start mb-4">
       <input
-        checked
         id="checkbox-1"
         type="checkbox"
         value=""
-        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+        class="w-10 h-10 outline-none text-blue-600 bg-gray-100 border-gray-300 rounded accent-black"
       />
       <label
         for="checkbox-1"
-        class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+        class="ml-2 text-xs font-normal text-gray-900 dark:text-gray-300"
       >
         {text}{" "}
         <a href="#" class="text-blue-600 hover:underline dark:text-blue-500">
