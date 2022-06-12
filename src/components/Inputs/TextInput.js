c7e84901603b747e87ee8881c336c0974a551775
tylerasa <sarpongasaresylvester@gmail.com>
2022-06-12 21:23:52 +0000

    text input component

---
+++ src/components/Inputs/TextInput.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const TextInput = ({ type, placeholder, label }) => {
+  return (
+    <div class="mb-6">
+      <label
+        for="email"
+        class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+      >
+        {label}
+      </label>
+      <input
+        type={type}
+        id="email"
+        class="bg-gray-50 outline-none border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder={placeholder}
+        required=""
+      />
+    </div>
+  );
+};
+
+export default TextInput;
