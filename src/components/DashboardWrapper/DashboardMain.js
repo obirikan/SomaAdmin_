5ece45304ec69f4e13fb5354412a23cf4097247f
tylerasa <sarpongasaresylvester@gmail.com>
2022-06-19 23:46:46 +0000

    class=> className

--- src/components/DashboardWrapper/DashboardMain.js
+++ src/components/DashboardWrapper/DashboardMain.js
@@ -3,11 +3,11 @@ import NavBar from "./NavBar";
 
 const DashboardMain = () => {
   return (
-    <main class="main flex flex-col flex-grow -ml-64 md:ml-0 transition-all duration-150 ease-in">
+    <main className="main flex flex-col flex-grow -ml-64 md:ml-0 transition-all duration-150 ease-in">
       <NavBar />
-      <div class="main-content flex flex-col flex-grow p-4">
-        <h1 class="font-bold text-2xl text-gray-700">Dashboard</h1>
-        <div class="flex flex-col flex-grow bg-white rounded mt-4">
+      <div className="main-content flex flex-col flex-grow p-4">
+        <h1 className="font-bold text-2xl text-gray-700">Dashboard</h1>
+        <div className="flex flex-col flex-grow bg-white rounded mt-4">
           Main
         </div>
       </div>
