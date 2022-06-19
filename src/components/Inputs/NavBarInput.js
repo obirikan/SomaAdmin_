dde3ba32edf730a00641e6aac1dcfa5a7e1c0eb3
tylerasa <sarpongasaresylvester@gmail.com>
2022-06-19 23:53:21 +0000

    +svg styling

--- src/components/Inputs/NavBarInput.js
+++ src/components/Inputs/NavBarInput.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { ReactComponent as Activation } from "../../assets/svgs/activation.svg";
 
 const NavBarInput = ({ opts }) => {
   return (
-    <div className="ml-5">
-      <select className="bg-gray-50 outline-none border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-primary block w-full p-2.5 ">
-        <p>2</p>
+    <div className="ml-5 relative">
+      <Activation className="absolute top-2.5 left-2.5" />
+      <select className="bg-gray-50 outline-none border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-primary block w-full p-2.5  pl-10">
         {opts.map((ele, i) => {
           return <option key={i}>{ele.text}</option>;
         })}
