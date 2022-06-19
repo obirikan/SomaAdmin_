b443ef090575faa6b73c31c6fb0cf6cb1de60fca
tylerasa <sarpongasaresylvester@gmail.com>
2022-06-19 23:22:08 +0000

    =dashboard main cleanup

--- src/components/DashboardWrapper/DashboardMain.js
+++ src/components/DashboardWrapper/DashboardMain.js
@@ -7,7 +7,7 @@ const DashboardMain = () => {
       <NavBar />
       <div class="main-content flex flex-col flex-grow p-4">
         <h1 class="font-bold text-2xl text-gray-700">Dashboard</h1>
-        <div class="flex flex-col flex-grow border-4 border-gray-400 border-dashed bg-white rounded mt-4">
+        <div class="flex flex-col flex-grow bg-white rounded mt-4">
           Main
         </div>
       </div>
