e5104bb2e5bf099f5c9e3e5d674f02a5081df0a2
tylerasa <sarpongasaresylvester@gmail.com>
2022-06-21 21:54:50 +0000

    +analytics

--- src/components/HomeTabs/index.js
+++ src/components/HomeTabs/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Tabs } from "flowbite-react";
+import Analytics from "./Analytics";
 const HomeTabs = () => {
   return (
     <Tabs.Group aria-label="Tabs with underline" style="underline">
-      <Tabs.Item active={true}  title="Analytics">Analytics</Tabs.Item>
-      <Tabs.Item title="Verifications">
-        Verifications
+      <Tabs.Item active={true} title="Analytics">
+        <Analytics />
       </Tabs.Item>
+      <Tabs.Item title="Verifications">Verifications</Tabs.Item>
     </Tabs.Group>
   );
 };
