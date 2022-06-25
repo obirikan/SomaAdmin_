c9634dd44538ed044e0742ef78a5a2d4a025fea9
tylerasa <sarpongasaresylvester@gmail.com>
2022-06-25 09:56:33 +0000

    +moving dashboard main content to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <main className="main flex flex-col flex-grow -ml-64 md:ml-0 transition-all duration-150 ease-in">
+      <NavBar />
+      <div className="main-content flex flex-col flex-grow p-4">
+        <div className="flex flex-col flex-grow bg-white rounded mt-4">
+          <HomeTabs />
+        </div>
+      </div>
+    </main>
+  );
+};
 
-export default Home
+export default Home;
