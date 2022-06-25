fdeb17b09875cc0148c1d2a7e4394cc057c407a4
tylerasa <sarpongasaresylvester@gmail.com>
2022-06-25 10:14:44 +0000

    +passing in title prop

--- src/components/DashboardWrapper/NavBar.js
+++ src/components/DashboardWrapper/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import NavBarInput from "../Inputs/NavBarInput";
 
-const NavBar = () => {
+const NavBar = ({ title }) => {
   const options = [
     {
       text: "Yellow Solution",
@@ -19,7 +19,7 @@ const NavBar = () => {
   return (
     <header className="header bg-white shadow py-4 px-4">
       <div className="header-content flex items-center flex-row">
-        <span className=" font-semibold text-2xl">Home</span>
+        <span className=" font-semibold text-2xl">{title}</span>
         <div className="flex ml-auto items-center">
           <div className="flex items-center justify-between">
             <span className="w-3 h-3 bg-appRed shadow-redShadow  rounded-full block mr-2"></span>
