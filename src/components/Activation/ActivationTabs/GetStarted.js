2e5167294e5d0120cdedacd80406a10fa9484f77
tylerasa <sarpongasaresylvester@gmail.com>
2022-06-27 09:24:45 +0000

    +get started

--- src/components/Activation/ActivationTabs/GetStarted.js
+++ src/components/Activation/ActivationTabs/GetStarted.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const GetStarted = () => {
   return (
-    <div>GetStarted</div>
-  )
-}
+    <div className="m-auto w-fit mt-10">
+      <h6 className="font-bold">Activate your Rattify account</h6>
+      <p>Activate your account to start verifying IDs in production</p>
+      <button
+        type="button"
+        className="text-white flex bg-primary  font-medium rounded-md text-sm px-3 py-2.5 mr-2 mb-2 mt-10"
+      >
+        Get started
+      </button>
+    </div>
+  );
+};
 
-export default GetStarted
+export default GetStarted;
