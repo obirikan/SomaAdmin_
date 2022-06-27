af466e072db8251f8cc83d049e4a3e4827bf00a0
tylerasa <sarpongasaresylvester@gmail.com>
2022-06-27 22:15:53 +0000

    +padding: 24px

--- src/components/Activation/ActivationTabs/Company.js
+++ src/components/Activation/ActivationTabs/Company.js
@@ -1,11 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import { ReactComponent as BlueArrow } from "../../../assets/svgs/blue-arrow.svg";
+import CompanyForm from '../Company/CompanyForm'
 const Company = () => {
   return (
-    <div className='box-shadow'>
-
+    <div className="w-full m-auto md:w-[50rem] bg-white shadow-companyShadow p-6 ">
+      <span className="flex items-center">
+        <BlueArrow />
+        <span className="text-primary ml-2 text-sm cursor-pointer  x">
+          Back
+        </span>
+      </span>
+      <h3 className="font-semibold mt-5">Let us know about your company</h3>
+      <p className="mt-5 text-sm">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <CompanyForm/>
     </div>
-  )
-}
+  );
+};
 
-export default Company
+export default Company;
