96d825b9cb44e9b8e532c6f525022cd43f037bb6
tylerasa <sarpongasaresylvester@gmail.com>
2022-07-02 13:19:45 +0000

    +verification link

--- src/components/Integrations/IntegrationsTabs/GetStarted.js
+++ src/components/Integrations/IntegrationsTabs/GetStarted.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Integration from "../GetStarted/Integration";
 import Sandbox from "../GetStarted/Sandbox";
+import VerificationLink from "../GetStarted/VerificationLink";
+import { ReactComponent as WhiteArrowRight } from "../../../assets/svgs/white-arrow-right.svg";
 
 const GetStarted = () => {
   return (
     <div className="w-full m-auto md:w-[44rem] ">
-    <Sandbox/>
-    <Integration/>
+      <Sandbox />
+      <Integration />
+      <VerificationLink />
+      <button
+        type="button"
+        className="text-white flex bg-primary  font-medium rounded-md text-sm px-3 py-2.5 mr-2 mb-2 mt-10"
+      >
+        <span className="flex justify-center items-center">
+          Next <WhiteArrowRight className="ml-2" />
+        </span>
+      </button>
     </div>
   );
 };
